6a930abf486c153490d1cea47143483ebfa0996e
valhayot <valeriehayot@gmail.com>
2018-07-20 22:38:22 +0000

    automated master batch script creation

---
+++ example/dummyprogram.py
@@ -0,0 +1,27 @@
+from pyspark import SparkContext, SparkConf
+from random import randint
+import argparse, time
+
+def sleep_task(x):
+    time.sleep(randint(0, 2))
+    return (randint(0, 5), x)
+
+def main():
+
+    conf = SparkConf().setAppName("Spark Wait")
+    sc = SparkContext.getOrCreate(conf=conf)
+
+    parser = argparse.ArgumentParser(description='Dummy program to help test pilot scheduling')
+    parser.add_argument('-p', '--partitions', type=int, help="number of partitions")
+    args = parser.parse_args()
+
+    waitRDD = sc.parallelize([x for x in range(0,1000)], args.partitions) \
+                .map(lambda x: sleep_task(x)) \
+                .reduceByKey(lambda x,y: x+y) \
+                .map(lambda x: sleep_task(x[1])) \
+                .reduceByKey(lambda x,y: x+y) \
+                .map(lambda x: sleep_task(x[1])) \
+                .collect()
+
+if __name__ == '__main__':
+    main()
---
+++ pilotspark.py
@@ -0,0 +1,40 @@
+import argparse, time, json
+from datetime import datetime
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Pilot-Agent scheduling for SLURM')
+    parser.add_argument('conf', type=argparse.FileType('r'), help="SLURM batch script (JSON)")
+    args = parser.parse_args()
+
+    conf = None
+    with args.conf as f:
+        conf = json.load(f)
+
+    master_fn = "master-{}.sh".format(datetime.now().strftime("%Y%m%d-%H%M%S%f"))
+    
+    with open(master_fn, "w") as master:
+        master.write("#!/bin/bash\n")
+        
+        for param in conf["master"]["sbatch"]:
+            master.write("#SBATCH {0}={1}\n".format(param["id"], param["value"])) 
+
+        master.write("\n\n")
+    
+        for path in conf["path"]:
+            master.write("export {0}={1}\n".format(path["id"], path["value"])) 
+
+        
+        # start master
+        master.write("export SPARK_MASTER=$(grep -Po 'spark://.*' $($SPARK_HOME/sbin/start-master.sh | grep -Po '/.*out')) \n")
+
+        # run master
+        master.write("srun -n 1 -N 1 $SPARK_HOME/bin/spark-submit --master $SPARK_MASTER {}\n".format(conf["master"]["program"]))
+        
+        # stop program
+        master.write("$SPARK_HOME/sbin/stop-all.sh")        
+        
+
+
+if __name__ == '__main__':
+    main()
