060cc46255783a745ca01f5d2a5cd866d940b680
valhayot <valeriehayot@gmail.com>
2018-08-25 17:14:11 -0700

    fixed spark standalone on HPC

--- example/hpc_template.sh
+++ example/hpc_template.sh
@@ -3,17 +3,22 @@
 echo start $(date +%s.%N) > $mstr_bench
 echo 'NODE: ' $(hostname)
 
-module load spark
-module load python
+module load spark/2.3.0
+#module load python
 
-start-master.sh
+export SPARK_WORKER_DIR=$SLURM_TMPDIR
+
+$SPARK_HOME/sbin/start-master.sh
 if [ ! -f $mstr_log ]; then
     lockfile -r 0 $mstr_lock
     if [ $? -eq 0 ]; then
-        MASTER_URI=$(curl -s http://localhost:8080/json/ | jq -r ".url")
+	while [ -z "$MASTER_URI" ]
+	do
+		MASTER_URI=$(curl -s http://localhost:8080/json/ | jq -r ".url")
+		sleep 5
+	done
         echo $MASTER_URI > $mstr_log
-        ssh $head_ip spark-submit --master $MASTER_URI --executor-cores=${SLURM_CPUS_PER_TASK} --executor-memory=${SLURM_MEM_PER_NODE}M $spscript&
-        rm -f $mstr_lock
+        #ssh $head_ip "spark-submit --master $MASTER_URI --executor-cores=${SLURM_CPUS_PER_TASK} --executor-memory=${SLURM_MEM_PER_NODE}M $spscript&"
     else
         while [ ! -f $mstr_log ]; do sleep 5; done
         MASTER_URI=$(head -n 1 $mstr_log)
@@ -24,14 +29,14 @@ fi
 
 echo 'RUNNING MASTER: ' $MASTER_URI
 
-start-slave.sh $MASTER_URI
+$SPARK_HOME/sbin/start-slave.sh $MASTER_URI
 
 # added sleep because the node's status is "ALIVE" initially
 # needs to be fixed
-sleep 200
-if [[ $(curl -s http://localhost:8080/json/ | jq -r ".status") == "ALIVE" ]]; then
-    stop-slave.sh
-    stop-master.sh
-    echo 'TERMINATED' >> $mstr_log
-fi
+while [[ $(tail -n 1 $mstr_log) != "SUCEEDED" ]]; do
+	sleep 5
+done
+$SPARK_HOME/sbin/stop-slave.sh
+$SPARK_HOME/sbin/stop-master.sh
+rm -f $mstr_lock
 echo end $(date +%s.%N) >> $mstr_bench
--- pilotspark.py
+++ pilotspark.py
@@ -1,7 +1,8 @@
 from os import path as op
-import argparse, time, json, subprocess, os
+import argparse, time, json, os
 from datetime import datetime
 from slurmpy import Slurm
+from subprocess import Popen, PIPE
 
 def main():
 
@@ -16,8 +17,10 @@ def main():
     with args.params as f:
         conf = json.load(f)
 
-    if args.yarn:
+    if args.yarn and 'HADOOP_HOME' in os.environ:
         open(op.join(os.environ['HADOOP_HOME'], 'etc/hadoop/slaves'), 'w').close()
+    elif args.yarn:
+        open(op.join(conf["COMPUTE"]["HADOOP_HOME"], 'etc/hadoop/slaves'), 'w').close()
 
     submit_func = "bash" if args.no_submit else "sbatch"
 
@@ -40,7 +43,32 @@ def main():
         # SLURM batch submit workers
         s.run(args.template, cmd_kwargs=conf["COMPUTE"], _cmd=submit_func)
         
-        
+
+    while not op.isfile(conf["COMPUTE"]["mstr_log"]):
+        time.sleep(5)
+
+    master_url = ""
+
+    with open(conf["COMPUTE"]["mstr_log"], 'r') as f:
+        master_url = f.readline().strip('\n')
+
+    fw = open("tmpout", "wb")
+    fr = open("tmpout", "r")
+    p = Popen(conf["DRIVER"]["slurm_alloc"], stdin = PIPE, stdout = fw, stderr = fw, bufsize = 1)
+
+    for module in conf["DRIVER"]["modules"]:
+        p.stdin.write("module load {}\n".format(module).encode('utf-8'))
+
+    program = ("spark-submit --master {0} --executor-cores=${{SLURM_CPUS_PER_TASK}} "
+               "--executor-memory=${{SLURM_MEM_PER_NODE}}M {1}\n").format(master_url, conf["DRIVER"]["program"])
+
+    p.stdin.write(program.encode('utf-8'))
+    p.stdin.write("echo 'SUCCEEDED' >> {}".format(conf["COMPUTE"]["mstr_log"]).encode('utf-8'))
+    out = fr.read()
+    fw.close()
+    fr.close()
+
+
 
 if __name__ == '__main__':
     main()
