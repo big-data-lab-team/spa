485dbee084ef7d4e9b22bf50ac68cf89c7c95ce1
valhayot <valeriehayot@gmail.com>
2019-01-26 22:06:32 -0500

    fixed scala application bugs

--- example/dummyscala/src/main/scala/IncrementApp.scala
+++ example/dummyscala/src/main/scala/IncrementApp.scala
@@ -13,11 +13,39 @@ object IncrementApp {
     Usage: increment bb_dir output_dir iterations [--delay] [--work_dir]
   """
 
-  def readImg( filename:String, data:PortableDataStream ) : Byte = {
+  def readImg( filename:String, data:PortableDataStream )
+    : Tuple2[String, Array[Array[Array[Array[Double]]]]] = {
     val niftibytes = data.open()
-    val nifti = NiftiHeader.read(niftibytes, filename)
+    val volume = NiftiVolume.read(niftibytes, filename)
     niftibytes.close()
-    return data.toArray()(2)
+    return (new File(filename).getName(), volume.data.toArray())
+  }
+
+  def incrementData( filename:String, data: Array[Array[Array[Array[Double]]]], sleep: Int )
+    : Tuple2[String, Array[Array[Array[Array[Double]]]]] = {
+    for( i <- 0 to data.length - 1) {
+      for( j <- 0 to data(0).length - 1) {
+        for( k <- 0 to data(0)(0).length - 1) {
+          for( l <- 0 to data(0)(0)(0).length - 1 ) {
+            data(i)(j)(k)(l) = data(i)(j)(k)(l) + 1.0
+          }
+        }
+      }
+    }
+    Thread.sleep(sleep * 1000)
+    return (filename, data)
+  }
+
+  def saveData( fn: String, data: Array[Array[Array[Array[Double]]]] )
+    : Tuple2[String, String] = {
+    val volume = new NiftiVolume(data)
+
+    // hardcoded for now
+    volume.header.setDatatype(NiftiHeader.NIFTI_TYPE_UINT16)
+
+    volume.write(fn)
+
+    return (fn, "SUCCESS")
   }
 
   def main(args: Array[String]) {
@@ -34,7 +62,7 @@ object IncrementApp {
         case "--work_dir" :: string :: tail =>
                                 nextArgument(map ++ Map('work_dir -> string), tail)
         case "--delay" :: value :: tail =>
-                                nextArgument(map ++ Map('delay -> value.toFloat), tail)
+                                nextArgument(map ++ Map('delay -> value.toInt), tail)
         case string :: tail if arglist.size - tail.size == 1  =>
                                 nextArgument(map ++ Map('bb_dir -> string), tail)
         case string :: tail if arglist.size - tail.size == 2 =>
@@ -53,18 +81,21 @@ object IncrementApp {
     val delay = if ((options get 'delay).isEmpty) 0 else (options get 'delay).get
     val of = new File(options('output_dir).asInstanceOf[String])
     val output_dir = of.getAbsolutePath()
+    val its = options('iterations).asInstanceOf[Int]
     of.mkdirs()
     println(output_dir)
 
-    val imRDD = sc.binaryFiles(options('bb_dir).asInstanceOf[String])
-                  .map(x => readImg(x._1, x._2)).first() 
-    println(imRDD)                              
+    var imRDD = sc.binaryFiles(options('bb_dir).asInstanceOf[String])
+                  .map(x => readImg(x._1, x._2))
 
-    //val spark = SparkSession.builder.appName("Increment Application").getOrCreate()
-    //val logData = spark.read.textFile(logFile).cache()
-    //val numAs = logData.filter(line => line.contains("a")).count()
-    //val numBs = logData.filter(line => line.contains("b")).count()
-    //println(s"Lines with a: $numAs, Lines with b: $numBs")
-    //spark.stop()
+    for ( i <- 1 to its ) {
+      imRDD = imRDD.map(x => incrementData(x._1, x._2, delay.asInstanceOf[Int]))
+    }
+
+    val result = imRDD.map( x => saveData(new File(output_dir, x._1).getAbsolutePath(), x._2) ).collect()
+
+    result.foreach(x => println(x._1 + ": " + x._2))                              
+
+    sc.stop()
   }
 }
